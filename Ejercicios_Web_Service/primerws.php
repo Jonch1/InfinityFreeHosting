81ee4251f530e8dbf27fd3a9235d7c17455981cb
Juanjo <juanjose.s435736@cesurformacion.com>
2025-01-14 09:33:07 +0100

    Formularios con JS y PHP

--- Ejercicios_Web_Service/primerws.php
+++ Ejercicios_Web_Service/primerws.php
@@ -1,21 +1,27 @@
 <?php
 
-header('Content-type: application/json');
+    $method = $_SERVER['REQUEST_METHOD'];
+    $url = $_SERVER['REQUEST_URI'];
+    $parts = explode('/', $url);
+    $entrypoint = end($parts);
 
-$method = $_SERVER['REQUEST_METHOD'];
-$url = parse_url($_SERVER['REQUEST_URI']);
-$parts = explode('/', $url['path']);
-$entrypoint = end($parts);
+    if ($entrypoint === "test") {
 
-switch ($method) {
-    case 'GET':
-        echo json_encode(["message" => "Operación realizada correctamente"]);
-        break;
+        switch ($method) {
+            case 'GET':
+                echo json_encode(["message" => "Operación realizada correctamente"]);
+                break;
 
-    case 'POST':
-        http_response_code(405);
-        echo json_encode(["message" => "Método no permitido"]);
-        break;
-}
+            case 'POST':
+                http_response_code(405);
+                echo json_encode(["message" => "Método no permitido"]);
+                break;
+
+        }
+    }else {
+        echo "No has seleccionado un entrypoint";
+        http_response_code(404);
+    }
+
 ?>
 
